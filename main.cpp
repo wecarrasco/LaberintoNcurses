f671e1a42ddb98ae79cba2bd556813f61c94b5ba
Walther Carrasco <waemcame@hotmail.es>
2015-06-09 20:58:42 -0600

    V1

--- main.cpp
+++ main.cpp
@@ -19,6 +19,7 @@ int main()
 	char menu = getch();
 	if (menu - 48 == 1)
 	{
+		clear();
 		printTable();
 	}
 /*	char ch = getch();					//Wait for user input 
@@ -75,7 +76,7 @@ void printTable(){
 	int entrada = 1 + rand()%14;
 	int salida = 1 + rand()%14;
 
-	tabla[entrada][0] = ' ';
+	tabla[entrada][0] = '*';
 	tabla[salida][14] = ' ';
 
 	for (int i = 0; i < 15; i++)
@@ -85,6 +86,89 @@ void printTable(){
 			printw("[%c]",tabla[i][j]);
 		}
 		printw("\n");
+	}
+	char key = 0;
+	while(key != 'x'){
+		int x, y;
+		key = getch();
+
+		switch(key)
+		{	case 'a':
+			
+			for (int i = 0; i < 15; i++)
+			{
+				for (int j = 0; j < 15; j++)
+				{
+					if (tabla[i][j] == '*')
+					{
+						x = i;
+						y = j;
+					}
+				}
+			}
+			tabla[x][y] = ' ';
+			tabla[x][y-1] = '*';
+			break;
+			case 'd':
+			//int x, y;
+			for (int i = 0; i < 15; i++)
+			{
+				for (int j = 0; j < 15; j++)
+				{
+					if (tabla[i][j] == '*')
+					{
+						x = i;
+						y = j;
+					}
+				}
+			}
+			tabla[x][y] = ' ';
+			tabla[x][y+1] = '*';
+			break;
+			case 'w':
+			//int x, y;
+			for (int i = 0; i < 15; i++)
+			{
+				for (int j = 0; j < 15; j++)
+				{
+					if (tabla[i][j] == '*')
+					{
+						x = i;
+						y = j;
+					}
+				}
+			}
+			tabla[x][y] = ' ';
+			tabla[x-1][y] = '*';
+			break;
+			case 's':
+			//int x, y;
+			for (int i = 0; i < 15; i++)
+			{
+				for (int j = 0; j < 15; j++)
+				{
+					if (tabla[i][j] == '*')
+					{
+						addch('h');
+						x = i;
+						y = j;
+					}
+				}
+			}
+			tabla[x][y] = ' ';
+			tabla[x+1][y] = '*';
+			break;	
+		}
+		clear();
+		for (int i = 0; i < 15; i++)
+		{
+			for (int j = 0; j < 15; j++)
+			{
+				printw("[%c]",tabla[i][j]);
+			}
+			printw("\n");
+		}
+		printw("[%c]",key);
 	}
 
 	refresh();
